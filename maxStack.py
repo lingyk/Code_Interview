c3c206180142784579924e7c8ecebba2de5ee7cb
lingyk <lingyk1995@gmail.com>
2020-12-29 02:18:10 -0500

    maxstack

---
+++ maxStack.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def __init__(self):
+        self.stack = []
+        self.maxes = []
+
+    def push(self, val):
+        self.stack.append(val)
+        if self.maxes and self.maxes[-1] > val:
+            self.maxes.append(self.maxes[-1])
+        else:
+            self.maxes.append(val)
+
+    def pop(self):
+        if self.maxes:
+            self.maxes.pop()
+        return self.stack.pop()
+
+    def max(self):
+        return self.maxes[-1]
+
+s = Solution()
+s.push(1)
+s.push(2)
+s.push(3)
+s.push(2)
+print(s.pop())
