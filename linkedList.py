ba982a36a6f8d151d4ed01e08bb3ace4a7a2b16b
lingyk <lingyk1995@gmail.com>
2020-12-29 01:16:19 -0500

    update

--- linkedList.py
+++ linkedList.py
@@ -1,1 +1,28 @@
-class 
+class Node(object):
+    def __init__(self, val, next = None):
+        self.val = val
+        self.next = next
+
+    def __repr__(self):
+        res = str(self.val)
+        if self.next:
+            res += str(self.next)
+        return res
+class Solution(object):
+    def reverse(self, node):
+        curr = node
+        prev = None
+
+        while curr != None:
+            tmp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = tmp
+        return prev
+
+node = Node(1)
+node.next = Node(2)
+node.next.next = Node(3)
+node.next.next.next = Node(4)
+node.next.next.next.next = Node(5)
+print(Solution().reverse(node))
